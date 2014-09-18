f7602041c92ca5cb5a99f3fdb7013ac2e0874eb9
Vikram Ramakrishnan <lord.ezar@gmail.com>
2014-09-18 08:17:45 -0400

    updated for hamming test

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -2,10 +2,8 @@ class Hamming
   def self.compute(string1, string2)
     count = 0
     i = 0
-    while  i < [string1.length, string2.length].min
-      if string1[i] != string2[i]
-        count += 1
-      end
+    while string1[i] && string2[i]
+      count += 1 if string1[i] != string2[i]
       i += 1
     end
     count
